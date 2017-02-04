d02a56169574952cbc93cb57482506237f5cc2d0
matt-how <m.o.howard@outlook.com>
2017-02-04 22:57:52 +0000

    Soundcloud support added

--- app/src/main/java/com/succsoftware/grack/MainActivity.java
+++ app/src/main/java/com/succsoftware/grack/MainActivity.java
@@ -2,12 +2,41 @@ package com.succsoftware.grack;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import java.util.*;
+import java.io.*;
+
 
 public class MainActivity extends AppCompatActivity {
-
+    ArrayList<Song> queue = new ArrayList<Song>();
+    Song current;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        WebView myWebView = (WebView) findViewById(R.id.webview);
+        myWebView.setWebViewClient(new WebViewClient());
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        queue.add(new Song("https://soundcloud.com/majorleaguewobs/filthy-frank-theme"));
+        //while(true){
+            current = queue.remove(0);
+            current.getDetails();
+            switch (current.getType()){
+                case 0:
+
+                case 1:
+
+                case 2:
+                    myWebView.loadUrl(current.getDomain());
+            }
+        //}
+
     }
+
+
 }
--- app/src/main/java/com/succsoftware/grack/Song.java
+++ app/src/main/java/com/succsoftware/grack/Song.java
@@ -5,17 +5,33 @@ package com.succsoftware.grack;
  */
 
 public class Song {
-    int type; //0 local mp3, 1 spotify, 2 soundcloud
-    String title;
-    String artist;
+    private int type; //0 local mp3, 1 spotify, 2 soundcloud
+    private String title;
+    private String artist;
+    private String domain;
 
-    public void getDetails(String data){
-        if(data.toLowerCase().contains("spotify")){
+    public Song(String domain){
+        this.domain = domain;
+    }
+
+    public void getDetails(){
+        if(domain.toLowerCase().contains("spotify")){
             type = 1;
         }
-        else if(data.toLowerCase().contains("soundcloud")){
+        else if(domain.toLowerCase().contains("soundcloud")){
             type = 2;
+
+        }
+        else{
+            type=0;
         }
     }
 
+    public int getType(){
+        return (type);
+    }
+
+    public String getDomain() {
+        return domain;
+    }
 }
